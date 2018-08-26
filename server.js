1afa5b8d664fedc28848300ef5834f058d762608
Javier <jdistefano138@gmail.com>
2018-08-26 15:23:11 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+//Requires
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const weather = require('./weatherlib');
+
+//Variables de entorno
+process.env.PORT = process.env.PORT || 3000;
+
+
+//Configuración para acceder al body
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+
+//Respuesta del post
+app.post('/', function(req, res) {
+    let body = req.body;
+    let texto;
+
+    location = encodeURI(body.location);
+    weather.currentWeather(location, function(clima) {
+        let resp = JSON.parse(clima);
+        texto = `La temperatura actual en ${resp.location.name} es de ${resp.current.temp_c}º Celsius`;
+
+        res.json({
+            speech: texto,
+            displayText: texto,
+            source: 'team info'
+        });
+    });
+
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto: ", process.env.PORT);
+});
